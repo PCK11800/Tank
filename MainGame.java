c4c83c06300d189a141cf25ca57e63244137afe6
PCK11800 <32240017+PCK11800@users.noreply.github.com>
2019-11-09 13:01:28 +0000

    Add files via upload

--- MainGame.java
+++ MainGame.java
@@ -1,3 +1,5 @@
+import java.awt.MouseInfo;
+
 public class MainGame{
 
     GameWindow gameWindow;
@@ -13,6 +15,7 @@ public class MainGame{
     private void gameLoop(){
         createPlayerTank();
         while(true){
+            setConstantTurretDirection(tank);
             gamePanel.repaint();
         }
     }
@@ -35,6 +38,27 @@ public class MainGame{
 
         gameWindow.initKeyListener(tank, gamePanel);
     }
+
+    //This works!!
+    private void setConstantTurretDirection(Tank tank){
+        int mouseXPos = MouseInfo.getPointerInfo().getLocation().x;
+        int mouseYPos = MouseInfo.getPointerInfo().getLocation().y;
+
+        double dx = tank.getHull().getxPos() - mouseXPos;
+        double dy = tank.getHull().getyPos() - mouseYPos;
+
+        double inRads = Math.atan2(dy, dx);
+
+        if(inRads < 0){
+            inRads = Math.abs(inRads);
+        }
+        else{
+            inRads = 2 * Math.PI - inRads;
+        }
+
+        tank.getTurret().rotateObject(- Math.toDegrees(inRads) - 90);
+    }
+
     public static void main(String[] args){
         MainGame main = new MainGame();
         main.initializeGameEnviroment();
